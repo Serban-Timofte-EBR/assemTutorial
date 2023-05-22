f42865a94550c1b6fc87122f87a1f885799724c4
Timofte Serban Gabriel <108286629+Serban-Timofte-EBR@users.noreply.github.com>
2023-05-22 07:24:19 +0300

    Update osproject.sh

--- osproject.sh
+++ osproject.sh
@@ -1,21 +1,30 @@
-sumaparelor (){
-echo "Primul numar: "
-read num1
-echo "Al doilea numar: "
-read num2
-echo "Al treilea numar: "
-read num3
-echo "Al patrulea numer: "
-read num4
+sumaparelor() {
+    echo "Primul numar: "
+    read num1
+    echo "Al doilea numar: "
+    read num2
+    echo "Al treilea numar: "
+    read num3
+    echo "Al patrulea numar: "
+    read num4
 
-sum=0
-for num in "$num1" "$num2" "$num3" "$num4"
-do
+    sum=0
+    pare=()
+
+    for num in "$num1" "$num2" "$num3" "$num4"
+    do
         if [[ $((num%2)) -eq 0 ]]
         then
-                sum=$((sum + num))
+            sum=$((sum + num))
+            pare+=($num)
         fi
-done
+    done
+
+    pare=($(echo "${pare[@]}" | tr ' ' '\n' | sort -n))
+    pare_crescator=$(IFS=, ; echo "${pare[*]}")
+
+    echo "Suma numerelor pare este: $sum"
+    echo "Numerele pare in ordine crescatoare sunt: $pare_crescator"
 }
 
 sumaparelor
